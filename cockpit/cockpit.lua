4d9de2c8ec3170aca7c69d29e55c6e39f56d08c0
Klayton Kowalski <klaytonkowalski@gmail.com>
2024-02-24 11:21:24 -0600

    Added checkbox and radio utilities. Added enabled state. Added custom inputs.

--- cockpit/cockpit.lua
+++ cockpit/cockpit.lua
@@ -45,47 +45,43 @@ local action_dy = nil
 
 local components = {}
 
+local input =
+{
+	down = { action_id = hash("mouse_button_left"), value = 0 }
+}
+
 --------------------------------------------------------------------------------
 -- Local Variables
 --------------------------------------------------------------------------------
 
 local function uncheck_radio_group(group)
 	for node, component in pairs(components) do
-		if component.type == type_radio and component.group == group and component.checked then
-			component.checked = false
-			if component.callback then
-				component.callback(node, component.over, component.down, true)
+		if component.enabled then
+			if component.type == type_radio and component.group == group and component.checked then
+				component.checked = false
+				if component.callback then
+					component.callback(node, component.over, component.down, true)
+				end
 			end
 		end
-	end
-end
-
-local function update_component_data(component)
-	if component.type == type_button then
-		-- todo: possibly nothing?
-	elseif component.type == type_checkbox then
-		component.checked = not component.checked
-	elseif component.type == type_radio then
-		if not component.checked then
-			uncheck_radio_group(component.group)
-		end
-		component.checked = not component.checked
 	end
 end
 
 local function over_callback()
 	for node, component in pairs(components) do
-		if gui.pick_node(node, action_x, action_y) then
-			if not component.over then
-				component.over = true
+		if component.enabled then
+			if gui.pick_node(node, action_x, action_y) then
+				if not component.over then
+					component.over = true
+					if component.callback then
+						component.callback(node, component.over, component.down, false)
+					end
+				end
+			elseif component.over then
+				component.over = false
 				if component.callback then
 					component.callback(node, component.over, component.down, false)
 				end
-			end
-		elseif component.over then
-			component.over = false
-			if component.callback then
-				component.callback(node, component.over, component.down, false)
 			end
 		end
 	end
@@ -93,10 +89,12 @@ end
 
 local function down_callback()
 	for node, component in pairs(components) do
-		if component.over then
-			component.down = true
-			if component.callback then
-				component.callback(node, component.over, component.down, false)
+		if component.enabled then
+			if component.over then
+				component.down = true
+				if component.callback then
+					component.callback(node, component.over, component.down, false)
+				end
 			end
 		end
 	end
@@ -104,11 +102,20 @@ end
 
 local function up_callback()
 	for node, component in pairs(components) do
-		if component.down then
-			component.down = false
-			if component.over and component.callback then
-				update_component_data(component)
-				component.callback(node, component.over, component.down, true)
+		if component.enabled then
+			if component.down then
+				component.down = false
+				if component.over and component.callback then
+					if component.type == type_checkbox then
+						component.checked = not component.checked
+					elseif component.type == type_radio then
+						if not component.checked then
+							uncheck_radio_group(component.group)
+						end
+						component.checked = not component.checked
+					end
+					component.callback(node, component.over, component.down, true)
+				end
 			end
 		end
 	end
@@ -118,6 +125,14 @@ end
 -- Module Functions
 --------------------------------------------------------------------------------
 
+function cockpit.set_action_ids(action_ids)
+	for key, value in pairs(input) do
+		if action_ids[key] then
+			value.action_id = action_ids[key]
+		end
+	end
+end
+
 function cockpit.on_input(action_id, action)
 	if not action_id then
 		action_x = action.x
@@ -126,14 +141,24 @@ function cockpit.on_input(action_id, action)
 		action_dy = action.dy
 		over_callback(action)
 	elseif action.pressed then
-		if action_id == hash("mouse_button_left") then
+		if action_id == input.down.action_id then
+			input.down.value = 1
 			down_callback()
 		end
 	elseif action.released then
-		if action_id == hash("mouse_button_left") then
+		if action_id == input.down.action_id then
+			input.down.value = 0
 			up_callback()
 		end
 	end
+end
+
+function cockpit.set_component_enabled(node, enabled)
+	components[node].enabled = enabled
+end
+
+function cockpit.get_component_enabled(node)
+	return components[node].enabled
 end
 
 -- Button ----------------------------------------------------------------------
@@ -142,6 +167,7 @@ function cockpit.create_button(node, callback)
 	components[node] =
 	{
 		type = type_button,
+		enabled = true,
 		node = node,
 		callback = callback,
 		over = false,
@@ -158,6 +184,7 @@ function cockpit.create_checkbox(node, callback, checked)
 	components[node] =
 	{
 		type = type_checkbox,
+		enabled = true,
 		node = node,
 		callback = callback,
 		over = false,
@@ -165,15 +192,20 @@ function cockpit.create_checkbox(node, callback, checked)
 		checked = checked or false
 	}
 	if callback then
-		callback(node, components[node].over, components[node].down, false)
+		callback(node, components[node].over, components[node].down, checked)
 	end
 end
 
-function cockpit.get_checkbox_data(node)
-	return
-	{
-		checked = components[node].checked
-	}
+function cockpit.set_checkbox_checked(node, checked)
+	if checked == components[node].checked then
+		return
+	end
+	components[node].checked = checked
+	components[node].callback(node, component.over, component.down, true)
+end
+
+function cockpit.get_checkbox_checked(node)
+	return components[node].checked
 end
 
 -- Radio -----------------------------------------------------------------------
@@ -182,6 +214,7 @@ function cockpit.create_radio(node, callback, group, checked)
 	components[node] =
 	{
 		type = type_radio,
+		enabled = true,
 		node = node,
 		callback = callback,
 		over = false,
@@ -189,16 +222,27 @@ function cockpit.create_radio(node, callback, group, checked)
 		group = group,
 		checked = checked or false
 	}
+	if checked then
+		uncheck_radio_group(group)
+	end
 	if callback then
-		callback(node, components[node].over, components[node].down, false)
+		callback(node, components[node].over, components[node].down, checked)
 	end
 end
 
-function cockpit.get_radio_data(node)
-	return
-	{
-		checked = components[node].checked
-	}
+function cockpit.set_radio_checked(node, checked)
+	if checked == components[node].checked then
+		return
+	end
+	if checked then
+		uncheck_radio_group(component.group)
+	end
+	components[node].checked = checked
+	components[node].callback(node, component.over, component.down, true)
+end
+
+function cockpit.get_radio_checked(node)
+	return components[node].checked
 end
 
 return cockpit
